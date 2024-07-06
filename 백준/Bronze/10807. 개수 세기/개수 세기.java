04ba0a7b8ae7e68ece15146d0898b2bbc70793a4
j0chan <141411728+j0chan@users.noreply.github.com>
2024-07-06 21:01:21 +0900

    [Bronze V] Title: 개수 세기, Time: 112 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,28 +1,31 @@
-import java.io.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
-
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
 
-        for(int i = 0 ; i<n ; i++){
-            arr[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int v = sc.nextInt();
+        int v = Integer.parseInt(br.readLine());
         int count = 0;
 
-        for(int j = 0 ; j<n ; j++){
-            if(arr[j]==v){
-                count ++;
+        for (int j = 0; j < n; j++) {
+            if (arr[j] == v) {
+                count++;
             }
         }
 
         System.out.println(count);
 
+        br.close();
     }
 }
