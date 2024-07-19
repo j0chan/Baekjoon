06bdcf16b54d52f66f05d9193fc94d32b1a9a93f
j0chan <141411728+j0chan@users.noreply.github.com>
2024-07-19 22:15:47 +0900

    [Bronze III] Title: 최댓값, Time: 236 ms, Memory: 18476 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args){
+class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i,j;
-        int max = 0;
-        int x = 1, y = 1;
+        int[][] arr = new int[9][9];
 
-        int arr[][] = new int[9][9];
-
-        for(i=0;i<9;i++) {
-            for(j=0;j<9;j++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
 
-        for(i=0;i<9;i++) {
-            for(j=0;j<9;j++) {
-                if(arr[i][j]>max) {
+        int maxN = 1;
+        int maxM = 1;
+        int max = 0;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (max < arr[i][j]) {
                     max = arr[i][j];
-                    x = i+1;
-                    y = j+1;
+                    maxN = i + 1;
+                    maxM = j + 1;
                 }
             }
         }
-        sc.close();
-
         System.out.println(max);
-        System.out.println(x+" "+y);
+        System.out.println(maxN + " " + maxM);
+
     }
 }
