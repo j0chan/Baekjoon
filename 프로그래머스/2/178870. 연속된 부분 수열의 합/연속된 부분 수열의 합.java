82c4b5f4955203f9e126868b40c5bcd114643a63
j0chan <141411728+j0chan@users.noreply.github.com>
2024-08-04 19:44:19 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 21.40 ms, Memory: 137 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -0,0 +1,38 @@
+/*
+    left=0, right=1부터 시작
+    l+r이 
+*/
+
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = new int[]{0,sequence.length -1};
+        
+        int left=0, right=1; // 포인터 초기화
+        
+        int sum = sequence[0];
+        
+        while(left<right) {
+            if(sum==k){ 
+                change(left, right, answer); // change 호출해서 결과 변경
+                sum = sum-sequence[left++]; // left 우측으로 한 칸 이동
+            } else if(sum>k){
+                sum = sum-sequence[left++]; // left 우측으로 한 칸 이동
+            } else if(right<sequence.length){
+                sum = sum+sequence[right++]; // right 우측으로 한 칸 이동
+            } else{ // right포인터가 배열 끝에 도달 시
+                break; // 루프 종료
+            }
+        }
+        
+        return answer;
+    }
+    
+    // answer배열을 갱신하는 메서드
+    public void change(int left, int right, int[] answer) {
+        // 현재 배열 길이 < 기존 저장된 배열 길이
+        if((right-1-left)<(answer[1]-answer[0])){ 
+            answer[0] =left;
+            answer[1] = right-1;
+        }
+    }
+}
