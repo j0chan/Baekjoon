3fa7ec034e280efbfc9b595a5a5bffc49d9f7d5d
j0chan <141411728+j0chan@users.noreply.github.com>
2024-08-29 19:37:26 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -5,6 +5,10 @@ import java.io.InputStreamReader;
 /*
     1,2,3을 이용해 더 큰 수를 만드는 원리이므로
     1,2,3을 만드는 경우의 수를 파악하고 점화식을 구한다
+
+    n을 만드는 방법은 n-3, n-2, n-1을 만드는 경우의 수의 합과 같다.
+    e.g) 4를 만드는 방법
+    1,2,3을 만드는 경우의 수의 합, 즉 1+2+4=7
  */
 public class Main { // 123더하기
     public static void main(String[] args) throws IOException {
@@ -17,11 +21,7 @@ public class Main { // 123더하기
         arr[2] = 2; // 2를 만드는 방법 2가지
         arr[3] = 4; // 3을 만드는 방법 4가지
 
-        /*
-            n을 만드는 방법은 n-3, n-2, n-1을 만드는 경우의 수의 합과 같다.
-            e.g) 4를 만드는 방법
-            1,2,3을 만드는 경우의 수의 합, 즉 1+2+4=7
-         */
+        // 점화식을 이용해 n을 만드는 경우의 수를 배열에 저장
         for (int i = 4; i <= 11; i++) {
             arr[i] = arr[i-3] + arr[i-2] + arr[i-1];
         }
